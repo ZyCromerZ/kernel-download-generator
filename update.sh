0010f9e578855228862a7e9debf7d8f1cfc8dfb4
ZyCromerZ <neetroid97@gmail.com>
2021-03-08 17:01:10 +0700

    update to fix error
    Signed-off-by: ZyCromerZ <neetroid97@gmail.com>

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
+{[ -z "$KDType" ]} && KDType="$1"
 
-git reset --hard
-git branch -D $1
+git branch -D "${KDType}"
 
 if [ "$1" == "stock-clang" ];then
     NameKernel="Stock CFW Clang"
@@ -38,18 +38,20 @@ fi
 if [ -z "$NameKernel" ] && [ -z "$NameKernel" ];then
     echo 'oops . . .'
 else
-    echo "$KernelFiles" 
-    SHA1CHECK=$(sha1sum "${KernelFiles}" | cut -d' ' -f1)
-    git checkout -b $1
-    sed -i "s/"'name": .*'"/"'name": "'"${NameKernel}"'",'"/g" "$JsonLocation"
-    if [[ "$1" == *"stock-memeui"*  ]];then
-        sed -i "s/"'link": "link-kernel.*'"/"'link": "https:\/\/osdn.dl.osdn.net\/storage\/g\/z\/zy\/zyc-kernels\/Begonia\/Memeui\/Stock\/'"${GetCBD}\/${ZipName}"'",'"/g" "$JsonLocation"
-    elif [[ "$1" == *"stock"*  ]];then
-        sed -i "s/"'link": "link-kernel.*'"/"'link": "https:\/\/osdn.dl.osdn.net\/storage\/g\/z\/zy\/zyc-kernels\/Begonia\/AOSP-CFW\/Stock\/'"${GetCBD}\/${ZipName}"'",'"/g" "$JsonLocation"
+    if [ ! -z "$KDType" ];then
+        echo "$KernelFiles" 
+        SHA1CHECK=$(sha1sum "${KernelFiles}" | cut -d' ' -f1)
+        git checkout -b "${KDType}"
+        sed -i "s/"'name": .*'"/"'name": "'"${NameKernel}"'",'"/g" "$JsonLocation"
+        if [[ "$1" == *"stock-memeui"*  ]];then
+            sed -i "s/"'link": "link-kernel.*'"/"'link": "https:\/\/osdn.dl.osdn.net\/storage\/g\/z\/zy\/zyc-kernels\/Begonia\/Memeui\/Stock\/'"${GetCBD}\/${ZipName}"'",'"/g" "$JsonLocation"
+        elif [[ "$1" == *"stock"*  ]];then
+            sed -i "s/"'link": "link-kernel.*'"/"'link": "https:\/\/osdn.dl.osdn.net\/storage\/g\/z\/zy\/zyc-kernels\/Begonia\/AOSP-CFW\/Stock\/'"${GetCBD}\/${ZipName}"'",'"/g" "$JsonLocation"
+        fi
+        sed -i "s/"'date": .*'"/"'date": "'"${GetCBD}"'",'"/g" "$JsonLocation"
+        sed -i "s/"'version": .*'"/"'version": "'"${KVer}-${HeadCommitId}"'",'"/g" "$JsonLocation"
+        sed -i "s/"'sha1": .*'"/"'sha1": "'"${SHA1CHECK}"'"'"/g" "$JsonLocation"
+        git add . && git commit -s -m 'update' && git push -f origin "${KDType}"
+        git checkout master
     fi
-    sed -i "s/"'date": .*'"/"'date": "'"${GetCBD}"'",'"/g" "$JsonLocation"
-    sed -i "s/"'version": .*'"/"'version": "'"${KVer}-${HeadCommitId}"'",'"/g" "$JsonLocation"
-    sed -i "s/"'sha1": .*'"/"'sha1": "'"${SHA1CHECK}"'"'"/g" "$JsonLocation"
-    git add . && git commit -s -m 'update' && git push -f origin $1
-    git checkout master
 fi
